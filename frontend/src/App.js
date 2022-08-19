7ec9238ef8c648b44422b3bdd3f3f57cd725f242
CamGham <greayham@gmail.com>
2022-08-19 14:36:42 +1200

    tfinished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,13 @@ import Home from "./pages/Home";
 import SEPractice from "./pages/SE-Practice";
 import SubmitArticle from "./pages/Submit-Article";
 import NotFoundPage from "./pages/404";
-import { Route, NavLink, BrowserRouter as Router, Routes, Navigate } from "react-router-dom";
+import {
+  Route,
+  NavLink,
+  BrowserRouter as Router,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
 const App = () => {
   return (
@@ -25,12 +31,11 @@ const App = () => {
         </ul>
         <div className="content">
           <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/SEPractice" element={<SEPractice/>} />
-          <Route path="/SubmitArticle" element={<SubmitArticle/>} />
-          <Route path="/404" element={<NotFoundPage/>}/>
-          <Route path="*" element={<Navigate to="/404" />}/>
-          
+            <Route path="/" element={<Home />} />
+            <Route path="/SEPractice" element={<SEPractice />} />
+            <Route path="/SubmitArticle" element={<SubmitArticle />} />
+            <Route path="/404" element={<NotFoundPage />} />
+            <Route path="*" element={<Navigate to="/404" />} />
           </Routes>
         </div>
       </div>
